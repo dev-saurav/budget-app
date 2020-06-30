d9c496a21290115daa2fd2fdd80623c46cf41701
webDevSaurav <webdevelopersaurav@gmail.com>
2020-07-01 00:04:38 +0530

    final build

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,7 +5,8 @@ import { getLocalStroage } from '../utils/utilFunc'
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-    const [state, dispatch] = useReducer(Reducer, getLocalStroage());
+    const initState = getLocalStroage() ? getLocalStroage() : { incomeTrans: [], expenseTrans: [] }
+    const [state, dispatch] = useReducer(Reducer, initState);
 
     return (
         <AppContext.Provider value={{ state, dispatch }}>
