44901be5878ad98db5c4f0ea032b09c71d77485a
webDevSaurav <webdevelopersaurav@gmail.com>
2020-06-30 23:57:39 +0530

    Updates

---
+++ precache-manifest.181a04e3659270051262ee82284aa20b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c89957e84e974566f089ab3e13f4c3b",
+    "url": "/budget-app/index.html"
+  },
+  {
+    "revision": "257edbb956f557d0f6d5",
+    "url": "/budget-app/static/css/main.cbf2e276.chunk.css"
+  },
+  {
+    "revision": "f1a33474adc02ab39458",
+    "url": "/budget-app/static/js/2.5a058ae3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-app/static/js/2.5a058ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257edbb956f557d0f6d5",
+    "url": "/budget-app/static/js/main.8cd49c11.chunk.js"
+  },
+  {
+    "revision": "ddef51c8f7043137a866",
+    "url": "/budget-app/static/js/runtime-main.48d5e94f.js"
+  }
+]);
